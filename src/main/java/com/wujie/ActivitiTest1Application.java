c05437fb10823255ef2c3bf95dc938a01b598c52
new-Object <13753110545@163.com>
2017-11-30 09:59:13 +0800

    activiti练习

---
+++ src/main/java/com/wujie/ActivitiTest1Application.java
@@ -0,0 +1,12 @@
+package com.wujie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiTest1Application.class, args);
+	}
+}
