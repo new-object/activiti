55be4a1961bb888cb46a1bab87958d249fb5067b
new-object <32151972+new-object@users.noreply.github.com>
2017-11-30 10:16:16 +0800

    Update ActivitiTest1Application.java

--- src/main/java/com/wujie/ActivitiTest1Application.java
+++ src/main/java/com/wujie/ActivitiTest1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActivitiTest1Application {
-
+//activiti测试
 	public static void main(String[] args) {
 		SpringApplication.run(ActivitiTest1Application.class, args);
 	}
